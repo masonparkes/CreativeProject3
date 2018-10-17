9ba6ba17651abe52b95df823f34ae8f0358dfa84
Mason <masonlparkes@gmail.com>
2018-10-17 00:08:33 +0000

    basic functionality complete?

--- Creative.js
+++ Creative.js
@@ -10,19 +10,38 @@ angular.module('bigThing', [])
                     text: stuff.reason,
                     weight: parseInt(stuff.weight,10)
                     });
-                $scope.Entry.reason="next Item";
-                $scope.Entry.weight="importance from 1-10";
+                $scope.Entry.reason="";
+                $scope.Entry.weight="";
             };
             $scope.addPro = function(stuff) {
                 $scope.pros.push({
                     text: stuff.reason,
                     weight: parseInt(stuff.weight,10)
                     });
-                $scope.Entry.reason="next Item";
-                $scope.Entry.weight="importance from 1-10";
+                $scope.Entry.reason="";
+                $scope.Entry.weight="";
             };
             $scope.done = function(){
-                //Console.log("winner");
+                var proCount=0;
+                var conCount=0;
+                for (var i=0;i<$scope.pros.length;i++)
+                    
+                   {var pro=$scope.pros[i];
+                       proCount+=pro.weight};
+                for (i=0;i<$scope.cons.length;i++)
+                    {var con=$scope.cons[i];
+                    conCount+=con.weight};
+                if (conCount<proCount)
+                {$scope.message="You should do it!"}
+                else if (proCount<conCount)
+                {$scope.message="You might regret it!"}
+                else
+                {$scope.message="Pros and Cons are equally matched! What will tip the balance?"};
+            }
+            $scope.clear=function(){
+                $scope.cons=[];
+                $scope.pros=[];
+                $scope.message="";
             }
         }
 
