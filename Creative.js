2be1d72c15f565a6c6c12d36935a08bfec507c64
Josh Hunt <jshunt196@gmail.com>
2018-10-19 04:26:45 +0000

    Josh's changes

--- Creative.js
+++ Creative.js
@@ -5,21 +5,34 @@ angular.module('bigThing', [])
             $scope.pros=[];
             $scope.cons=[];
             //Console.log("this");
+            
             $scope.addCon = function(stuff) {
-                $scope.cons.push({
-                    text: stuff.reason,
-                    weight: parseInt(stuff.weight,10)
-                    });
-                $scope.Entry.reason="";
-                $scope.Entry.weight="";
+                if (!(Number.isInteger($scope.Entry.weight)) && ($scope.Entry.weight > 10
+                    || $scope.Entry.weight < 1)) {
+                    alert("Value must be an integer between 1 and 10!");
+                }
+                else {
+                    $scope.cons.push({
+                        text: stuff.reason + " (" + stuff.weight + ")",
+                        weight: parseInt(stuff.weight,10)
+                        });
+                    $scope.Entry.reason="";
+                    $scope.Entry.weight="";
+                }
             };
             $scope.addPro = function(stuff) {
-                $scope.pros.push({
-                    text: stuff.reason,
-                    weight: parseInt(stuff.weight,10)
-                    });
-                $scope.Entry.reason="";
-                $scope.Entry.weight="";
+                if (!(Number.isInteger($scope.Entry.weight)) && ($scope.Entry.weight > 10
+                    || $scope.Entry.weight < 1)) {
+                    alert("Value must be an integer between 1 and 10!");
+                }
+                else {
+                    $scope.pros.push({
+                        text: stuff.reason + " (" + stuff.weight + ")",
+                        weight: parseInt(stuff.weight,10)
+                        });
+                    $scope.Entry.reason="";
+                    $scope.Entry.weight="";
+                }
             };
             $scope.done = function(){
                 var proCount=0;
@@ -31,7 +44,12 @@ angular.module('bigThing', [])
                 for (i=0;i<$scope.cons.length;i++)
                     {var con=$scope.cons[i];
                     conCount+=con.weight};
-                if (conCount<proCount)
+                alert('cons: ' + conCount + " and pros: " + proCount);
+                if (conCount<(proCount/2))
+                {$scope.message="Definitely do it!"}
+                else if (conCount>=proCount - 1)
+                {$scope.message="Maybe do it?"}
+                else if (conCount<proCount)
                 {$scope.message="You should do it!"}
                 else if (proCount<conCount)
                 {$scope.message="You might regret it!"}
